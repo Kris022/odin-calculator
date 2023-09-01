6e5a0a1c59da5b0cd6f0301f5b92a71239edacf4
Kris <kharonek@yahoo.com>
2023-08-31 21:24:39 -0300

    implement operate function

--- main.js
+++ main.js
@@ -1,1 +1,41 @@
-console.log("works")
+let numOne;
+let numTwo;
+let operator;
+
+function add(numOne, numTwo) {
+  return numOne + numTwo;
+}
+
+function subtract(numOne, numTwo) {
+  return numOne - numTwo;
+}
+
+function multiply(numOne, numTwo) {
+  return numOne * numTwo;
+}
+
+function divide(numOne, numTwo) {
+  return numOne / numTwo;
+}
+
+function operate(numOne, numTwo, operator) {
+  let result;
+  switch (operator) {
+    case "+":
+      result = add(numOne, numTwo);
+      break;
+    case "-":
+      result = subtract(numOne, numTwo);
+      break;
+    case "*":
+      result = multiply(numOne, numTwo);
+      break;
+    case "/":
+      result = divide(numOne, numTwo);
+      break;
+  }
+
+  return result;
+}
+
+console.log(operate(2, 3, "+"));
---
+++ utils.js
@@ -0,0 +1,15 @@
+function add() {
+    return
+}
+
+function subtract() {
+    return
+}
+
+function multiply() {
+    return
+}
+
+function divide() {
+    return
+}
